b1e3ad8d998c020423e41da1ec603138b3e56ca9
Ivine <ivine.b5@gmail.com>
2020-01-30 13:19:32 -0600

    updating objects tutorial

--- objects/objects.js
+++ objects/objects.js
@@ -8,4 +8,38 @@ console.log(evesCar);
 // mutate: add data (properties)
 evesCar.fuelInjection = true;
 console.log(evesCar);
+evesCar['4-wheel-drive'] = true;
 
+// dispaly single item with dot
+console.log('eves car is', evesCar.color);
+
+// display single item with array notation
+console.log('is it 4 wheel dig?', evesCar['4-wheel-drive']);
+
+// using toString (works with arrays too)
+console.log(evesCar.toString());
+const myCars = ['rav', 'pilot', 'juke'];
+console.log(myCars.toString());
+
+// mutate: remove property
+delete evesCar.pistons;
+console.log('eves car without pistons', evesCar);
+
+// merge two objects
+const basicJuke = {
+    wheels: 4,
+    color: 'red',
+    doors: 4,
+    make: 'Nissan'
+};
+const newJuke = Object.assign({}, basicJuke, evesCar);
+console.log('new juke', newJuke);
+
+// objects in arrays
+const cars = [
+    { make: 'Nissan', model: 'Rogue', color: 'orange' },
+    { make: 'Nissan', model: 'Altima', color: 'blue' },
+    { make: 'Dodge', model: 'Viper', color: 'blue' }
+
+];
+console.log('index 0 of cars color', cars[0].color);
