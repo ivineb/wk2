d5b5949afbc36a7fde9fd3bc03f7fa582d41d2fa
Ivine <ivine.b5@gmail.com>
2020-01-30 12:51:09 -0600

    add objects

---
+++ objects/objects.js
@@ -0,0 +1,11 @@
+// create
+const evesCar = {
+    color: 'black',
+    pistons: 4
+};
+console.log(evesCar);
+
+// mutate: add data (properties)
+evesCar.fuelInjection = true;
+console.log(evesCar);
+
