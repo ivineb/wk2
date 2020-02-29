23d36ae508e29e1df214fb9ffc6ef83199dd483d
Ivine <ivine.b5@gmail.com>
2020-02-29 13:13:02 -0600

    added v-if!
    forgot adding the v-if!

--- 6.a-vue-/script.js
+++ 6.a-vue-/script.js
@@ -17,16 +17,17 @@ var llamaFacts = new Vue({
 var yourLLamaFacts = new Vue({
   el: '#yourLLamaFacts',
   data: {
-    fact: 'Add your llama fact here!'
+    fact: 'Add your llama fact here!',
+    seen: true
   }
 })
 
 // third div here, regular way where i labeled the "expression" then gave it its value here (hope i am explaining correctly)
 var moreFacts = new Vue({
-    el: '#moreFacts',
-    data: {
-        growth: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.',
-        combat: 'Llamas don\'t bite. They spit when they\'re agitated, but that\'s mostly at each other.',
-        diet: 'Llamas are vegetarians and have very efficient digestive systems.'
-    }
+  el: '#moreFacts',
+  data: {
+    growth: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.',
+    combat: 'Llamas don\'t bite. They spit when they\'re agitated, but that\'s mostly at each other.',
+    diet: 'Llamas are vegetarians and have very efficient digestive systems.'
+  }
 });
